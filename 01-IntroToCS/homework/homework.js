cdf8cde806049a46150eb5372ed867d3aaf2cd26
lisandrosz <szyzuklisandro@gmail.com>
2022-09-12 16:50:18 -0300

    first homework done!

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,28 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  let acumulador = 0
+  let str = num.toString()
+  for(let i = 0; i < str.length; i++){
+    let valor = Number(str[(str.length - i) - 1])
+    acumulador += valor * (2**i)
+  }
+  return acumulador
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+let arr = [] 
+let str 
+while (true) {
+  arr.unshift(num % 2)
+  num = Math.floor(num / 2)
+  if(num === 1){
+    arr.unshift(1)
+    str = arr.join("")
+    break
+  }
+}
+return str
 }
 
 
