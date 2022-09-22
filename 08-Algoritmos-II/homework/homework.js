ad7738dfeb866fc82671a7b9210034d718df6eaa
lisandrosz <szyzuklisandro@gmail.com>
2022-09-22 15:49:59 -0300

    little changes

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -7,6 +7,23 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if (array.length < 2) return array;
+
+  let izq = [];
+  let der = [];
+  let eq = [];
+  let piv = Math.floor(Math.random() * array.length);
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[piv]) {
+      izq.push(array[i]);
+    } else if (array[i] > array[piv]) {
+      der.push(array[i]);
+    } else {
+      eq.push(array[i]);
+    }
+  }
+  return quickSort(izq).concat(eq).concat(quickSort(der));
 }
 
 function mergeSort(array) {
@@ -15,6 +32,41 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  function unir(izq, der) {
+    let nuevo = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < izq.length && j < der.length) {
+      if (izq[i] < der[j]) {
+        nuevo.push(izq[i]);
+        i++;
+      } else {
+        nuevo.push(der[j]);
+        j++;
+      }
+    }
+
+    return nuevo.concat(izq.slice(i)).concat(der.slice(j));
+  }
+
+  if (array.length < 2) {
+    return array;
+  }
+
+  let izq = [];
+  let der = [];
+  let mitad = Math.floor(array.length / 2);
+
+  for (let i = 0; i < array.length; i++) {
+    if (i < mitad) {
+      izq.push(array[i]);
+    } else {
+      der.push(array[i]);
+    }
+  }
+
+  return unir(mergeSort(izq), mergeSort(der));
 }
 
 // No modificar nada debajo de esta línea
