3cc58a138738dd3056a380a52d65fd960afb77e2
lisandrosz <szyzuklisandro@gmail.com>
2022-09-30 02:41:02 -0300

    hw ready, except extra credits

--- M2/03-ES6/homework/spec/selectorSpec.js
+++ M2/03-ES6/homework/spec/selectorSpec.js
@@ -1,28 +1,27 @@
-describe("selectorTypeMatcher", function() {
-  it("debe retornar el tipo 'id' para un selector de id", function() {
-    var type = selectorTypeMatcher('#pagetitle');
+describe("selectorTypeMatcher", function () {
+  it("debe retornar el tipo 'id' para un selector de id", function () {
+    var type = selectorTypeMatcher("#pagetitle");
     expect(type).toEqual("id");
   });
 
-  it("debe retornar el tipo 'class'  para un selector de clase", function() {
-    var type = selectorTypeMatcher('.image');
+  it("debe retornar el tipo 'class'  para un selector de clase", function () {
+    var type = selectorTypeMatcher(".image");
     expect(type).toEqual("class");
   });
 
-  it("debe retornar  el tipo 'tag.class' para un selector de tag.class", function() {
-    var type = selectorTypeMatcher('img.thumbnail');
+  it("debe retornar  el tipo 'tag.class' para un selector de tag.class", function () {
+    var type = selectorTypeMatcher("img.thumbnail");
     expect(type).toEqual("tag.class");
   });
 
-  it("debe retornar el tipo 'tag' para un selector de tag", function() {
-    var type = selectorTypeMatcher('div');
+  it("debe retornar el tipo 'tag' para un selector de tag", function () {
+    var type = selectorTypeMatcher("div");
     expect(type).toEqual("tag");
   });
 });
 
-describe("matchFunctionMaker", function() {
-
-  it("Debe retornar una funcion que matchie el ID que devuelve TRUE si el elemento matchea el ID", function() {
+describe("matchFunctionMaker", function () {
+  it("Debe retornar una funcion que matchie el ID que devuelve TRUE si el elemento matchea el ID", function () {
     var selector = "#price";
     var matcher = matchFunctionMaker(selector);
     var sampleDivEl = document.createElement("DIV");
@@ -30,7 +29,7 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleDivEl)).toEqual(true);
   });
 
-  it("debe retoranar una función que devuelve FALSE si el elemento no matchea el ID", function() {
+  it("debe retoranar una función que devuelve FALSE si el elemento no matchea el ID", function () {
     var selector = "#price";
     var matcher = matchFunctionMaker(selector);
     var sampleDivEl = document.createElement("DIV");
@@ -38,7 +37,7 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleDivEl)).toEqual(false);
   });
 
-  it("Debería retornar una funcion que matchie la CLASS que devuelve TRUE si el elemento matchea la className", function() {
+  it("Debería retornar una funcion que matchie la CLASS que devuelve TRUE si el elemento matchea la className", function () {
     var selector = ".heading";
     var matcher = matchFunctionMaker(selector);
     var sampleDivEl = document.createElement("DIV");
@@ -46,7 +45,7 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleDivEl)).toEqual(true);
   });
 
-  it("debería retornar una función que matchie la CLASS que devuelva TRUE si el elemento matche la classNAme, incluso cuando hay multiples clases en el elemento", function() {
+  it("debería retornar una función que matchie la CLASS que devuelva TRUE si el elemento matche la classNAme, incluso cuando hay multiples clases en el elemento", function () {
     var selector = ".heading";
     var matcher = matchFunctionMaker(selector);
     var sampleEl = document.createElement("H1");
@@ -54,7 +53,7 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleEl)).toEqual(true);
   });
 
-  it("deberia retornar una function que matchie la CLASS que devuelva FALSE si el elemento no matchie ningun className", function() {
+  it("deberia retornar una function que matchie la CLASS que devuelva FALSE si el elemento no matchie ningun className", function () {
     var selector = ".photo";
     var matcher = matchFunctionMaker(selector);
     var sampleEl = document.createElement("H1");
@@ -62,14 +61,14 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleEl)).toEqual(false);
   });
 
-  it("debería devolver un función que matchie el TAG que devuelva TRUE cuando el elemento matchea el tagName", function() {
-    var selector = 'div';
+  it("debería devolver un función que matchie el TAG que devuelva TRUE cuando el elemento matchea el tagName", function () {
+    var selector = "div";
     var matcher = matchFunctionMaker(selector);
     var sampleDivEl = document.createElement("div");
     expect(matcher(sampleDivEl)).toEqual(true);
   });
 
-  it("debería devolver un función que matchie el TAG.CLASS que devuelva TRUE cuando el elemento matchea el tagName AND Class", function() {
+  it("debería devolver un función que matchie el TAG.CLASS que devuelva TRUE cuando el elemento matchea el tagName AND Class", function () {
     var selector = "img.thumbnail";
     var matcher = matchFunctionMaker(selector);
     var sampleDivEl = document.createElement("img");
@@ -77,7 +76,7 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleDivEl)).toEqual(true);
   });
 
-  it("debería devolver un función que matchie el TAG.CLASS que devuelva FALSE si el elemento no matchea el tag", function() {
+  it("debería devolver un función que matchie el TAG.CLASS que devuelva FALSE si el elemento no matchea el tag", function () {
     var selector = "img.photo";
     var matcher = matchFunctionMaker(selector);
     var sampleEl = document.createElement("div");
@@ -85,72 +84,70 @@ describe("matchFunctionMaker", function() {
     expect(matcher(sampleEl)).toEqual(false);
   });
 
-  it("debería devolver un función que matchie el TAG.CLASS que devuelva FALSE si el elemento no matchea el className", function() {
+  it("debería devolver un función que matchie el TAG.CLASS que devuelva FALSE si el elemento no matchea el className", function () {
     var selector = "img.photo";
     var matcher = matchFunctionMaker(selector);
     var sampleEl = document.createElement("img");
     sampleEl.className = "photos lightback abstract"; // el elemento tiene tres distintas clases
     expect(matcher(sampleEl)).toEqual(false);
   });
-
 });
 
-describe("funcion de selector $ ", function() {
+describe("funcion de selector $ ", function () {
   var elements;
 
-  it("debe seleccionar un elemento por tag name (el root en este caso)", function() {
-    elements = $('body');
+  it("debe seleccionar un elemento por tag name (el root en este caso)", function () {
+    elements = $("body");
     expect(elements[0].tagName.toLowerCase()).toEqual("body");
   });
 
-  it("debería seleccionar un id", function() {
-    elements = $('#pagetitle');
+  it("debería seleccionar un id", function () {
+    elements = $("#pagetitle");
     expect(elements.length).toEqual(1);
   });
 
-  it("deberia seleccionar el elmento correcto por id", function() {
-    elements = $('#pagetitle');
+  it("deberia seleccionar el elmento correcto por id", function () {
+    elements = $("#pagetitle");
     expect(elements[0].innerHTML).toEqual("My Photos");
   });
 
-  it("debería seleccionar tag names", function() {
-    elements = $('h2');
+  it("debería seleccionar tag names", function () {
+    elements = $("h2");
     expect(elements.length).toEqual(3);
   });
 
-  it("debería seleccionar por clase", function() {
-    elements = $('.photo');
+  it("debería seleccionar por clase", function () {
+    elements = $(".photo");
     expect(elements.length).toEqual(4);
   });
 
-  it("deberia seleccionar por clase incluyendo elementos con multiples clases", function() {
-    elements = $('.lead');
+  it("deberia seleccionar por clase incluyendo elementos con multiples clases", function () {
+    elements = $(".lead");
     expect(elements.length).toEqual(3);
   });
 
-  it("deberia seleccionar por tag name y className", function() {
-    elements = $('h2.small');
+  it("deberia seleccionar por tag name y className", function () {
+    elements = $("h2.small");
     expect(elements.length).toEqual(2);
   });
-
 });
 
 describe("Credito Extra", function () {
-  describe("Selector de Jerarquía", function (){
+  describe("Selector de Jerarquía", function () {
     it("debe seleccionar los elementos correctos cuando el selector incluye a un child combinator (>)", function () {
-        elements = $('div > img');
-        expect(elements.length).toEqual(7);
+      elements = $("div > img");
+      expect(elements.length).toEqual(7);
 
-        elements = $('body > img');
-        expect(elements.length).toEqual(0);
+      elements = $("body > img");
+      expect(elements.length).toEqual(0);
     });
 
     it("debe seleccionar los elementos correctos cuando el selector incluye un descendant combinator (espacio en blanco)", function () {
-        elements = $('body p');
-        expect(elements.length).toEqual(2);
+      elements = $("body p");
+      expect(elements.length).toEqual(2);
 
-        elements = $('body img');
-        expect(elements.length).toEqual(7);
+      elements = $("body img");
+      expect(elements.length).toEqual(7);
     });
   });
 });
--- M2/03-ES6/homework/src/selector.js
+++ M2/03-ES6/homework/src/selector.js
@@ -1,4 +1,4 @@
-var traverseDomAndCollectElements = function(matchFunc, startEl) {
+var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
 
   if (typeof startEl === "undefined") {
@@ -9,16 +9,41 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  let nueva = function (matchFunc, startEl) {
+    if (matchFunc(startEl)) {
+      resultSet.push(startEl);
+    }
+    const hijos = startEl.children;
+    for (let i = 0; i < hijos.length; i++) {
+      if (hijos[i].hasChildNodes()) {
+        nueva(matchFunc, hijos[i]);
+      } else {
+        if (matchFunc(hijos[i])) {
+          resultSet.push([hijos[i]]);
+        }
+      }
+    }
+    return resultSet;
+  };
+  nueva(matchFunc, startEl);
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
-
-var selectorTypeMatcher = function(selector) {
+var selectorTypeMatcher = function (selector) {
   // tu código aquí
-  
+  let firstCharacter = selector[0];
+  if (firstCharacter === "#") return "id";
+  if (firstCharacter === ".") return "class";
+  else {
+    for (let i = 0; i < selector.length; i++) {
+      if (selector[i] === ".") return "tag.class";
+    }
+    return "tag";
+  }
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -26,22 +51,59 @@ var selectorTypeMatcher = function(selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function(selector) {
+var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
-  if (selectorType === "id") { 
-   
+  if (selectorType === "id") {
+    matchFunction = function (elemento) {
+      if ("#" + elemento.id.toLowerCase() === selector.toLowerCase()) {
+        return true;
+      } else {
+        return false;
+      }
+    };
   } else if (selectorType === "class") {
-    
+    matchFunction = function (elemento) {
+      if (elemento.classList.length > 1) {
+        const array = elemento.classList.values();
+        for (const value of array) {
+          if ("." + value === selector) return true;
+        }
+        return false;
+      } else {
+        if ("." + elemento.className === selector) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    };
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function (elemento) {
+      const arrSelector = selector.split(".");
+      if (arrSelector[0] === elemento.tagName.toLowerCase()) {
+        const array = elemento.classList.values();
+        for (const value of array) {
+          if (value === arrSelector[1]) return true;
+        }
+        return false;
+      } else {
+        return false;
+      }
+    };
   } else if (selectorType === "tag") {
-    
+    matchFunction = function (elemento) {
+      if (elemento.tagName.toLowerCase() === selector) {
+        return true;
+      } else {
+        return false;
+      }
+    };
   }
   return matchFunction;
 };
 
-var $ = function(selector) {
+var $ = function (selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
