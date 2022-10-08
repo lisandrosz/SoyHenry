bb9b7ae9e0cef10bbbee0a44b11915ae5e130005
lisandrosz <szyzuklisandro@gmail.com>
2022-10-07 22:16:33 -0300

    forms hw ready!

--- M2/10-React-Forms/homework/src/App.js
+++ M2/10-React-Forms/homework/src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import Form from './Form.jsx';
+import React from "react";
+import Form from "./Form.jsx";
 
 function App() {
-  return (
-    <Form />
-  );
+  return <Form />;
 }
 
 export default App;
--- M2/10-React-Forms/homework/src/Form.jsx
+++ M2/10-React-Forms/homework/src/Form.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function  Form() {
+export function validate(state) {
+  const errors = {};
+
+  if (state.username === "") {
+    errors["username"] = "Username is required";
+  } else if (!/\S+@\S+\.\S+/.test(state.username)) {
+    errors["username"] = "Username is invalid";
+  }
+
+  if (state.password === "") {
+    errors["password"] = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(state.password)) {
+    errors["password"] = "Password is invalid";
+  }
+
+  return errors;
+}
+
+export default function Form(props) {
+  const [state, setState] = React.useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = React.useState({});
+
+  const inputHandler = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+    setErrors(validate({ ...state, [event.target.name]: event.target.value }));
+  };
+
   return (
-      <div>
-        Componente Form
-      </div>
-  )
+    <div>
+      <form>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={state.username}
+            onChange={inputHandler}
+            className={errors.username && "danger"}
+          />
+          {errors.username && <p className="danger">{errors.username}</p>}
+        </div>
+        <br />
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={inputHandler}
+            className={errors.password && "danger"}
+          />
+          {errors.password && <p className="danger">{errors.password}</p>}
+        </div>
+        <br />
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
+  );
 }
