8d01ad999dabd19404d43ad95e5264dbbbf85ae9
lisandrosz <szyzuklisandro@gmail.com>
2022-10-20 18:04:27 -0300

    Cap 1 completed

--- M3/04-AdvancePromises/homework/src/pledge.js
+++ M3/04-AdvancePromises/homework/src/pledge.js
@@ -1,10 +1,32 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+const executor = function (resolve, reject) {};
 
+const $Promise = function (executor) {
+  if (typeof executor !== "function") {
+    throw new TypeError("executor is not a function");
+  }
+
+  this._state = "pending";
+  this._value;
+  this._internalResolve = (data) => {
+    if (this._state === "pending") {
+      this._value = data;
+      this._state = "fulfilled";
+    }
+  };
+  this._internalReject = (data) => {
+    if (this._state === "pending") {
+      this._value = data;
+      this._state = "rejected";
+    }
+  };
+  executor(this._internalResolve, this._internalReject);
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
