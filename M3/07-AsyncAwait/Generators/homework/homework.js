3a4cb654ddd9876d3d77682f850081e52015a555
lisandrosz <szyzuklisandro@gmail.com>
2022-10-26 20:40:26 -0300

    Generators hw ready

--- M3/07-AsyncAwait/Generators/homework/homework.js
+++ M3/07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,25 @@
-function fizzBuzzGenerator(max) {
-  // Tu código acá:
-
+function* fizzBuzzGenerator(max) {
+  let num = 1;
+  while (true) {
+    if (max) {
+      if (num > max) {
+        return;
+      }
+    }
+    if (num % 5 === 0 && num % 3 === 0) {
+      yield "Fizz Buzz";
+      num++;
+    } else if (num % 5 === 0) {
+      yield "Buzz";
+      num++;
+    } else if (num % 3 === 0) {
+      yield "Fizz";
+      num++;
+    } else {
+      yield num;
+      num++;
+    }
+  }
 }
 
 module.exports = fizzBuzzGenerator;
